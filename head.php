705cdc74e322211502d31c472d463825e052a9f8
root <ricardolau6@gmail.com>
2016-12-09 01:36:36 +0100

    Añado funcion bootstrap acordeon y mejor explicacion de pagina inicio .  Changes to be committed: 	modified:   head.php 	modified:   index.php 	new file:   js/bootstrap.min.js

--- head.php
+++ head.php
@@ -14,4 +14,5 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
 -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="<?php echo $HostNombre;?>/js/bootstrap.min.js"></script>
 
--- index.php
+++ index.php
@@ -14,9 +14,9 @@
 			<div class="col-md-8">
 				<h1>Tratamiento de imagenes</h1>
 				<h2>El objetivo de está aplicacion local </h2>
-				<p>Conseguir una aplicación totalmente independiente de virtuemar que :</p>
+				<p>Conseguir una aplicación que podamos gestionar las imagenes virtuemart :</p>
 				<ul>
-					<li>- Saber si las imagenes directorio product de virtuemart son utilizados por los productos que tenemos Base Datos.</li>
+					<li>- Saber que imagenes del directorio product de virtuemart son utilizados por los productos que tenemos Base Datos.</li>
 					<li>- Creando la imagenes miniatura de la imagenes de productos a la medida que le indiquemos, recortando la imagen y redimensionado.</li>
 				</ul>
 				<p>Lo ideal sería que fuera un componente a parte, para poder realizarlo directamente en la web, sin tener que tener un servidor local y realizar instalaciones locales de nuestra web.</p>
@@ -24,43 +24,47 @@
 				<strong>IDEAS:</strong>
 				<p>Crear un extension de virtuemart que realice este proceso.<strong>Dejaría ser independiente a virtuemart.</strong></p>
 				</div>
-				<h3>Controlar las imagenes que tenemos en virtuemart en productos</h3>
-				<p> Para poder controlar la imagenes de virtuemart tenemos crear una <strong>conexion a la base datos</strong>, ya que sino no podemos saber que imagenes están añadidas a los productos.</p>
-				<p> La conexion será a la base de datos local, aunque podría ser a la del <strong>servidor producción</strong>.</p>
-				<p> Si te indica un <strong>error de conexión</strong>, revisa el fichero configuracion, recuerda que el fichero se llama configuracion.php</p>
-			<div class="alert alert-info">
+				<div class="panel-group" id="accordion">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+						<h3 class="panel-title">
+							<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">¿Saber que imagenes realmente utilizamos en los productos?</a>
+						</h3>
+						</div>
+						<div id="collapseOne" class="panel-collapse collapse in">
+							<div class="panel-body">
+								<p> Para saber que imagenes utilizamos en los productos creamos una <strong>conexion a la base datos local</strong>.</p>
+								<p> Ver el fichero configuracion donde indicamos servidor, base de datos y usuario.</p>
+								<p> De momento no controlamos si realmente hay conexion, por lo que te recomiendo que revises log errores de servidor si ves que no funciona correctamente.</p>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="alert alert-info">
 				Al crear la miniaturas lo hace en directorio dentro <?php echo $DirImagRecortadas;?> , para evitar romper la web, por lo que se recomienda hacer una copia de seguridad antes pegar las imagenes en la web.
-			</div>
-			<div class="alert alert-info">
+				</div>
+				<div class="alert alert-info">
 				Recuerda que cuando sobreescribas las imagenes redimensionadas, tendras que limpiar cache del navegador para que puedas ver los cambios.
-			</div>
+				</div>
 			
 				
 			</div>
 			<div class="col-md-4">
 				<div>
-				<h2>Información funcionamiento</h2>
-				<p>Las funcionalidades que vamos hacer son: </p>
-				<ul>
-					<li> - Buscamos en BD de la web virtuemar si existen las imagenes que hay product </li>
-					<li> - Recortamos y optimizamos imagenes imagenes de product</li>
-				</ul>
-				</div>
-				<div>
 				<h2>Requesitos mínimos</h2>
 				<ul>
 				<li>Servidor Apache.</li>
 				<li>PHP y MySql</li>
-				<li>Framework Bootstrap</li>
 				</ul>
-				<p>Esta aplicación no tiene necesidad conexion a internet</p>
 				</div>
 				<div>
 					<div class="alert alert-info">
 					<p>Está aplicación es OPEN SOURCE, con ello queremos decir que puedes utilizar este código en otras aplicaciones y modificarlo sin problemas.</p>
 					</div>
 					<div class="alert alert-danger">
-					<p>Lo que no se puede es publicar son los datos de la base de datos y las imagenes para evitar problemas con el copyright si lo tiene.</p>
+					<p>Revisa el fichero configuracion para correcto funcionamiento.</p>
+					<p>No se publica el directorio de imagenes para evitar problemas con el copyright que puedan tener.</p>
 					</div>
 				</div>
 			</div>
